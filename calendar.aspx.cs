6c71982164e54f1013fadda0526737bdd790f621
Kiki Mac <eatlunchnow@users.noreply.github.com>
2017-04-13 11:18:32 -0700

    Update calendar.aspx.cs

--- calendar.aspx.cs
+++ calendar.aspx.cs
@@ -15,20 +15,43 @@ public partial class calendar : System.Web.UI.Page
     protected void Calendar_SelectionChanged(object sender, EventArgs e)
     {
         string myDate = Calendar.SelectedDate.ToShortDateString();
+        string schoolStart = "4/3/2017";
+        string petDay = "4/11/2017";
         string birthday = "5/9/2017";
+        string momDay = "5/14/2017";
+        string dadDay = "6/18/2017";
 
 
-        if (myDate.Equals(birthday))
+        if (myDate.Equals(schoolStart))
         {
-            Label.Text = "Happy Birthday!";
+            Label.Text = "Spring Quarter Starts";
+        }
 
+        else if (myDate.Equals(petDay))
+        {
+            Label.Text = "National Pet Day";
+        }
+        else if (myDate.Equals(birthday))
+        {
+            Label.Text = "My Birthday";
+
+        }
+        else if (myDate.Equals(momDay))
+        {
+            Label.Text = "Mother's Day";
+        }
+
+        else if (myDate.Equals(dadDay))
+        {
+            Label.Text = "Father's Day";
         }
         else
         {
             Label.Text = Calendar.SelectedDate.ToShortDateString();
         }
-     
 
     }
+
+  
 
 }
